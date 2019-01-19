e69528bc84284f86e92bfaced6f2fa9dbfbceb35
Nik Fedoseev <nik.fedoseev2011@yandex.ru>
2019-01-20 02:17:59 +0300

    sdssss

--- server/server.js
+++ server/server.js
@@ -8,4 +8,9 @@ app.get('/', function(req, res) {
   res.sendFile(path.resolve(__dirname, '../dist', 'index.html'));
 });
 
-app.listen(80, '0.0.0.0');
+app.set('port', (process.env.PORT || 5000));
+
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
